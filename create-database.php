112bd054d600b54a39af7daa1f0f6f20f848688f
Isaque O. Silva <Isaque1945@hotmail.com>
2021-02-09 12:19:44 -0300

    Implementação do arquivo para criação de um BD

---
+++ create-database.php
@@ -0,0 +1,30 @@
+<?php 
+    /* ARQUIVO USADO PARA CRIAR UM BANCO DE DADOS */
+
+    try {
+        /* VARIÁVEIS DE CONEXÃO */
+        $hostname = 'localhost'; /* HOST DE CONEXÃO */   
+        $username = 'root';      /* USUÁRIO P/ CONEXÃO */
+        $password = '';          /* SENHA P/ CONEXÃO */
+
+        /* STRING DE CONEXÃO */
+        $conn = new PDO('mysql:host='.$hostname,$username,$password);
+
+        /* AQUI O ATRIBUTO DE ERRO É CONVERTIDO PARA EXCEPTION(PDO)
+         PARA QUE ASSIM POSSA SER TRATADO NO BLOCO CATCH EM
+         CASO DE ERRO */
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        /* QUERY USADA PARA CRIAR UM BANCO DE DADOS(CLIENTES) */
+        $query = 'CREATE DATABASE clientes';
+
+        /* AQUI A FUNÇÃO EXEC É USADA PARA EXECUTAR
+         UMA DETERMINADA QUERY */
+        $conn->exec($query);
+
+        echo 'Banco de dados criado com sucesso!';
+    }
+    catch(PDOException $e) {
+        echo 'Erro, '.$e->getMessage();
+    }
+?>
